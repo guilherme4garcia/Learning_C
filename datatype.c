358feef58005528415db01686fb6ca7402cbcf93
Guilherme Garcia <guilherme4garcia@gmail.com>
2020-01-01 15:37:22 -0300

    update

---
+++ datatype.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+     int age = 40;  //integer
+     double gpa = 3.7;  //float
+     char grade = 'A'; // apenas 1 letra pode ser armazenada neste tipo
+     char phrase[] = "String of characters";  // double quotes, array, para armazenar string
+
+     return 0;
+}
---
+++ printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int favNum = 90;
+    char myChar = 'i';
+    printf("Hello\"World\n");
+    printf("My favorite %s is %d\n","number", favNum);
+    printf("My favorite letter is %c", myChar);
+
+    return 0;
+}
