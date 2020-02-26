8ada57daa1d1181d2d532dc1453a259373e66d0a
Guilherme Garcia <guilherme4garcia@gmail.com>
2020-02-26 16:26:46 -0300

    upload

--- urionline/Interval.c
+++ urionline/Interval.c
@@ -8,27 +8,27 @@ int main()
 
     if(n >= 0 && n <= 25.0000)
     {
-        printf("Intervalo [0,25]");
+        printf("Intervalo [0,25]\n");
     }
 
     if(n >= 25.00001 && n <= 50.0000000)
     {
-        printf("Intervalo (25, 50]");
+        printf("Intervalo (25,50]\n");
     }
 
      if(n >= 50.0000001 && n <= 75.0000000)
     {
-        printf("Intervalo (50, 75]");
+        printf("Intervalo (50,75]\n");
     }
 
      if(n >= 75.0000001 && n <= 100.0000000)
     {
-        printf("Intervalo (75, 100]");
+        printf("Intervalo (75,100]\n");
     }
 
      if(n < 0 || n > 100)
     {
-        printf("Fora de intervalo");
+        printf("Fora de intervalo\n");
     }
 
 
